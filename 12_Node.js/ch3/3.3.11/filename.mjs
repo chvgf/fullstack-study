692af97b3f70d65eeee357393f2491cb7e5b2064
chvgf <chvgf@naver.com>
2023-11-20 17:54:48 +0900

    a

--- 12_Node.js/ch3/3.3.11/filename.mjs
+++ 12_Node.js/ch3/3.3.11/filename.mjs
@@ -1,3 +1,3 @@
-console.log(import.meta.url); // 현재 파일 경로   폴더(디렉토리) 알아서 잘 추출해서 사용(뒤에 파일 이름,확장자 빼면 폴더)
+console.log(import.meta.url); // 현재 파일 경로   폴더는(디렉토리) 알아서 잘 추출해서 사용(뒤에 파일 이름,확장자 빼면 폴더)
 console.log('__filename은 에러');
 console.log(__filename); // 에러 발생
