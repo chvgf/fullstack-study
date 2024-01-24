8a9a4ae16bee644e27bf72d84afc58891d256da4
chvgf <chvgf@naver.com>
2024-01-24 19:47:19 +0900

    연습..

--- 11_React/my-shop/src/components/ProductListItem.js
+++ 11_React/my-shop/src/components/ProductListItem.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Col } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Col } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 // 커서포인터 주기 아이템에
 // 방법1: 스타일드 컴포넌트로 스타일 확장 Col 대신 이거 입력
@@ -11,18 +11,22 @@ import styled from 'styled-components';
 // 방법2: GlobalStyle에 공통 스타일로 만들어논 .커서-포인터 클래스 넣기
 
 function ProductListItem(props) {
-  const { productListItem: { title, price, imagePath, id } } = props;
+  const {
+    productListItem: { title, price, imagePath, id },
+  } = props;
   console.log(props.productListItem);
 
   const navigate = useNavigate();
 
   return (
-    <Col md={4} className='cursor-pointer'>
-      <img src= {imagePath} width="80%" 
+    <Col md={4} className="cursor-pointer">
+      <img
+        src={imagePath}
+        width="80%"
         // 상품 클릭 시 이동 경로 설정하기
         onClick={() => {
           // /detail/해당 상품 id
-          navigate(`/detail/${id}`)
+          navigate(`/detail/${id}`);
         }}
       />
       <h4>{title}</h4>
