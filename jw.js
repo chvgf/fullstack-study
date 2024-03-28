3748696e8162e3c73f6e3591124f251159f9bd79
chvgf <chvgf@naver.com>
2024-03-28 21:35:16 +0900

    let var 차이..실험

---
+++ jw.js
@@ -0,0 +1,60 @@
+// let aa = 111;
+// console.log(aa + "밖 위");
+// function test() {
+//   let aa = 222;
+//   console.log(aa + "함수안");
+// }
+// test();
+// console.log(aa + "밖 밑");
+
+// var aaa = 555;
+// console.log(aaa + "밖 위");
+// function vartest() {
+//   var aaa = 444;
+//   console.log(aaa + "함수안");
+// }
+// vartest();
+// console.log(aaa + "밖 밑");
+
+// let fooo = 1;
+// if (fooo + 1 === 2) {
+//   let fooo = 2;
+//   console.log("IF문 안의 foo : " + fooo);
+// }
+// console.log("IF문 밖의 foo : " + fooo);
+
+// var foo = 1;
+// if (foo + 1 === 2) {
+//   var foo = 2;
+//   console.log("IF문 안의 foo : " + foo);
+// }
+// console.log("IF문 밖의 foo : " + foo);
+
+// var ww = 1;
+// if (ww + 1 === 2) {
+//   var ww = 2;
+//   console.log(ww);
+// }
+// console.log(ww);
+
+// let www = 5;
+// if (www + 1 === 6) {
+//   let www = 6;
+//   console.log(www);
+// }
+// console.log(www);
+
+const a = {
+  a: "1",
+  b: "2",
+  c: "3",
+};
+console.log(a);
+a.a = "2";
+console.log(a);
+
+const b = ["1", "2", "3"];
+console.log(b);
+b.push("4");
+console.log(b);
+console.log(c);
